e3c3376e12ec868eb730d12cb7f9b0bd7896c50d
Daniel Abrilot <daniel.abrilot@gmail.com>
2014-01-06 16:13:21 -0200

    Todos :)

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -110,6 +110,187 @@ public class Interfaz {
 		lista.add(35);
 		lista.add(38);
 		cargo.addNewOperational(lista,coincidenciaGuardia, opera.get(5));
+		//Nivel tactico
+		//jefe de RRHH
+		String jeferrhh="JefeRRHH";
+	 	double coincidenciajeferrhh=62.5;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(0);
+	 	lista.add(2);
+	 	lista.add(12);
+	 	lista.add(13);
+	 	lista.add(31);
+	 	lista.add(27);
+	 	lista.add(39);
+	 	lista.add(35);
+	 	cargo.addNewOperational(lista, coincidenciajeferrhh, jeferrhh);
+	 	
+	 	//Director de finanzas
+	 	String dirfinanzas="DirectorFinanzas";
+	 	double coincidenciadirfinanzas=62.5;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(1);
+	 	lista.add(3);
+	 	lista.add(7);
+	 	lista.add(29);
+	 	lista.add(23);
+	 	lista.add(39);
+	 	lista.add(37);
+	 	lista.add(36);
+	 	cargo.addNewOperational(lista, coincidenciadirfinanzas, dirfinanzas);
+	 	
+	 	//Director de produccion
+	 	String dirproduccion="DirectorProduccion";
+	 	double coincidenciadirproduccion=62.5;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(1);
+	 	lista.add(9);
+	 	lista.add(11);
+	 	lista.add(29);
+	 	lista.add(28);
+	 	lista.add(34);
+	 	lista.add(39);
+	 	lista.add(36);
+	 	cargo.addNewOperational(lista, coincidenciadirproduccion, dirproduccion);
+	 	
+	 	//Director de informatica
+	 	String dirinformatica="DirectorInformatica";
+	 	double coincidenciadirinformatica=62.5;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(1);
+	 	lista.add(3);
+	 	lista.add(7);
+	 	lista.add(19);
+	 	lista.add(29);
+	 	lista.add(22);
+	 	lista.add(39);
+	 	lista.add(37);
+	 	cargo.addNewOperational(lista, coincidenciadirinformatica, dirinformatica);
+	 	
+	 	//Jefe de seccion
+	 	String jefeseccion="JefeSeccion";
+	 	double coincidenciajefeseccion=62.5;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(12);
+	 	lista.add(7);
+	 	lista.add(13);
+	 	lista.add(29);
+	 	lista.add(31);
+	 	lista.add(24);
+	 	lista.add(39);
+	 	lista.add(38);
+	 	cargo.addNewOperational(lista, coincidenciajefeseccion, jefeseccion);
+	 	
+	 	//Jefe de proyecto
+	 	String jefeproyecto="JefeProyecto";
+	 	double coincidenciajefeproyecto=71.4;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(1);
+	 	lista.add(4);
+	 	lista.add(12);
+	 	lista.add(19);
+	 	lista.add(29);
+	 	lista.add(39);
+	 	lista.add(35);
+	 	cargo.addNewOperational(lista, coincidenciajefeproyecto, jefeproyecto);
+	 	
+	 	//Nivel estrategico
+	 	//gerente general
+	 	String GerenteGeneral="GerenteGeneral";
+	 	double coincidenciaGerenteGeneral=90.9;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(6);
+	 	lista.add(11);
+	 	lista.add(4);
+	 	lista.add(9);
+	 	lista.add(20);
+	 	lista.add(29);
+	 	lista.add(32);
+	 	lista.add(18);
+	 	lista.add(24);
+	 	lista.add(39);
+	 	lista.add(36);
+	 	cargo.addNewOperational(lista, coincidenciaGerenteGeneral, GerenteGeneral);
+	 	
+	 	//gerente de finanzas
+	 	String GerenteFinanzas="GerenteFinanzas";
+	 	double coincidenciaGerenteFinanzas=87.5;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(1);
+	 	lista.add(3);
+	 	lista.add(7);
+	 	lista.add(13);
+	 	lista.add(34);
+	 	lista.add(39);
+	 	lista.add(38);
+	 	lista.add(36);
+	 	cargo.addNewOperational(lista, coincidenciaGerenteFinanzas, GerenteFinanzas);
+	 	
+	 	//Gerente de marketing
+	 	String GerenteMarketing="GerenteMarketing";
+	 	double coincidenciaGerenteMarketing=80;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(4);
+	 	lista.add(8);
+	 	lista.add(3);
+	 	lista.add(13);
+	 	lista.add(19);
+	 	lista.add(20);
+	 	lista.add(26);
+	 	lista.add(31);
+	 	lista.add(39);
+	 	lista.add(36);
+	 	cargo.addNewOperational(lista, coincidenciaGerenteMarketing, GerenteMarketing);
+	 	
+	 	//Gerente de ventas
+	 	String GerenteVentas="GerenteVentas";
+	 	double coincidenciaGerenteVentas=90;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(1);
+	 	lista.add(7);
+	 	lista.add(13);
+	 	lista.add(5);
+	 	lista.add(29);
+	 	lista.add(31);
+	 	lista.add(24);
+	 	lista.add(22);
+	 	lista.add(39);
+	 	lista.add(36);
+	 	cargo.addNewOperational(lista, coincidenciaGerenteVentas, GerenteVentas);
+	 	
+	 	//Gerente de producion
+	 	String GerenteProduccion="GerenteProduccion";
+	 	double coincidenciaGerenteProduccion=87.5;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(7);
+	 	lista.add(13);
+	 	lista.add(12);
+	 	lista.add(22);
+	 	lista.add(30);
+	 	lista.add(33);
+	 	lista.add(39);
+	 	lista.add(38);
+	 	cargo.addNewOperational(lista, coincidenciaGerenteProduccion, GerenteProduccion);
+	 	
+	 	//Presidente del directorio
+	 	String PresidenteDirectorio="PresidenteDirectorio";
+	 	double coincidenciaPresidenteDirectorio=100;
+	 	lista=new ArrayList<Integer>();
+	 	lista.add(1);
+	 	lista.add(4);
+	 	lista.add(8);
+	 	lista.add(5);
+	 	lista.add(9);
+	 	lista.add(11);
+	 	lista.add(13);
+	 	lista.add(17);
+	 	lista.add(19);
+	 	lista.add(20);
+	 	lista.add(22);
+	 	lista.add(24);
+	 	lista.add(39);
+	 	lista.add(38);
+	 	cargo.addNewOperational(lista, coincidenciaPresidenteDirectorio, PresidenteDirectorio);
 	}
 
 	/**
